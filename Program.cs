ef946b9f8863df7e3d2818db4b6cae29573c7c35
Maksym <hmel408757595@outlook.com>
2024-12-30 10:20:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace ExtentionMethod
+{
+    //Вариант № 1 функция ничего не возвращает
+    //public static class Extensions
+    //{
+    //    public static void Print(this string str)
+    //    {
+    //        Console.WriteLine(str);
+    //    }
+
+    //    public static void Print(this int number)
+    //    {
+    //        Console.WriteLine(number);
+    //    }
+
+    //    public static void Print(this double number)
+    //    {
+    //        Console.WriteLine(number);
+    //    }
+    //}
+    //Вариант № 2 распечатается объект и вернется его копия 
+    public static class Extensions2
+    {
+        public static string Print(this string str)
+        {
+            Console.WriteLine(str);
+            //return str;
+            return str.Replace(" ", "");
+        }
+
+        public static int Print(this int number)
+        {
+            Console.WriteLine(number);
+            return number;
+        }
+
+        public static double Print(this double number)
+        {
+            Console.WriteLine(number);
+            return number;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            "Hello world".Print();
+            2024.Print();
+            0.08.Print();
+        }
+    }
+}
